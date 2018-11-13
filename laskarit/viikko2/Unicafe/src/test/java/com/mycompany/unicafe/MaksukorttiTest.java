c4daae35221fa724accb217a7090a43dea75495e
terala <tomasterala@gmail.com>
2018-11-13 17:35:33 +0200

    Part 4 asti tehty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,43 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertTrue(kortti.saldo() == 10);
+    }
+    
+    @Test
+    public void rahanLatausKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertTrue(kortti.saldo()== 20);
+    }
+    
+    @Test
+    public void saldoVaheneeJosRahaaOn() {
+        kortti.otaRahaa(5);
+        assertTrue(kortti.saldo() == 5);
+    }
+    
+    @Test
+    public void saldoEiVaheneJosRahaaEiTarpeeksi() {
+        kortti.otaRahaa(15);
+        assertTrue(kortti.saldo() == 10);
+    }
+    
+    @Test
+    public void josRahatRiittaTrue() {
+        Kassapaate kassa = new Kassapaate();
+        Boolean riittiko = kassa.syoEdullisesti(kortti);
+        if (riittiko == true) {
+            assertEquals(true, riittiko);
+        } else {
+            assertEquals(false, riittiko);
+        }
+    }
+    
+    @Test
+    public void oikeaToString() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
 }
