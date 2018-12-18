616580c4bdf51ef68133d2acd19cf702e2e841e5
terala <tomasterala@gmail.com>
2018-12-18 19:25:48 +0200

    testei foodTable ja parannuksia muuten

---
+++ FoodTracker/test/foodtracker/utilities/TableFoodTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foodtracker.utilities;
+
+import java.time.LocalDate;
+import java.time.Month;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author terala
+ */
+public class TableFoodTest {
+    private TableFood tf;
+    private LocalDateConverter converter;
+    
+    @Before
+    public void setUp() {
+        this.tf = new TableFood(13, "name", "type", 69, "qType", LocalDate.now(), LocalDate.of(2020, Month.APRIL, 20));
+    }
+    
+    @Test
+    public void getIdTest() {
+        assertEquals("13", tf.getId());
+    }
+    
+    @Test
+    public void getFoodNameTest() {
+        assertEquals("name", tf.getFoodName());
+    }
+    
+    @Test
+    public void getFoodTypeTest() {
+        assertEquals("type", tf.getFoodType());
+    }
+    
+    @Test
+    public void getQuantityTest() {
+        assertEquals("69", tf.getQuantity());
+    }
+    
+    @Test
+    public void getQuantityTypeTest() {
+        assertEquals("qType", tf.getQuantityType());
+    }
+    
+//    @Test
+//    public void getDateAdded() {
+//        assertEquals(converter.dateToString(LocalDate.now()), tf.getDateAdded());
+//    }
+//    
+//    @Test
+//    public void getExpiration() {
+//        LocalDate date = LocalDate.of(2020, Month.APRIL, 20);
+//        assertEquals(converter.dateToString(date), tf.getExpirationDate());
+//    }
+}
