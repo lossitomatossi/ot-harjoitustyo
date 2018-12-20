dc8b30381662fae7663b691c234f1901b5a903a9
Tomas Terälä <35957699+lossitomatossi@users.noreply.github.com>
2018-12-20 13:25:08 +0200

    dataeditingview testaamista vailla

--- FoodTracker/src/foodtracker/ui/DataEditingView.java
+++ FoodTracker/src/foodtracker/ui/DataEditingView.java
@@ -3,14 +3,17 @@ package foodtracker.ui;
 import foodtracker.dao.FoodDao;
 import foodtracker.database.Database;
 import foodtracker.utilities.TableFood;
+import foodtracker.utilities.LocalDateConverter;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -22,6 +25,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class DataEditingView extends Application {
     private TableView<TableFood> table = new TableView<TableFood>();
@@ -30,12 +36,17 @@ public class DataEditingView extends Application {
     final HBox hb = new HBox();
 
     @Override
-    public void start(Stage primaryStage) throws ClassNotFoundException, SQLException {
+    public void start(Stage stage) throws ClassNotFoundException, SQLException {
+        Scene scene = new Scene(new Group());
+        stage.setTitle("Edit database");
+        stage.setWidth(600);
+        stage.setHeight(600);
         Database database = new Database("jdbc:sqlite:food.db");
         FoodDao allFoods = new FoodDao(database);
+        LocalDateConverter converter = new LocalDateConverter();
         
-        TableView<TableFood> table = new TableView();
-        table.setEditable(false);
+        table = new TableView();
+        table.setEditable(true);
         
         final Label label = new Label("Tracked Foods: ");
         label.setFont(new Font("Arial", 20));
@@ -62,7 +73,8 @@ public class DataEditingView extends Application {
         
         TableColumn columnExpiration = new TableColumn("Expiration");
         columnExpiration.setCellValueFactory(
-            new PropertyValueFactory<TableFood, String>("expirationDate"));
+            new PropertyValueFactory<TableFood, String>("expirationDate")
+        );
         
         //funktio create columns
         
@@ -94,21 +106,50 @@ public class DataEditingView extends Application {
         addAmountType.setPromptText("Unit of amount");
         final DatePicker addDate = new DatePicker();
         addDate.setMaxWidth(columnExpiration.getPrefWidth());
-        hb.getChildren().addAll(addFood, addType, addAmount, addAmountType, addDate);
+        
+        final Button addButton = new Button("Add");
+        addButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                String type = "";
+                if (addType.getText().startsWith("p")) {
+                    type = "pieces";
+                } else if (addType.getText().startsWith("g")){
+                    type = "grams";
+                } else if (addType.getText().startsWith("l")) {
+                    type = "liters";
+                } else {
+                    type = "unknown";
+                }
+                String date = converter.dateToString(addDate.getValue());
+                data.add(new TableFood(
+                        0,
+                        addFood.getText(),
+                        type,
+                        Integer.parseInt(addAmount.getText()),
+                        addAmountType.getText(),
+                        LocalDate.now(),
+                        addDate.getValue()));
+                        
+                addFood.clear();
+                addType.clear();
+                addAmount.clear();
+                addAmountType.clear();
+                addDate.setValue(null);
+            }
+        });
+        
+        hb.getChildren().addAll(addFood, addType, addAmount, addAmountType, addButton);
         hb.setSpacing(3);
         
         final VBox vbox = new VBox();
         vbox.setSpacing(5);
         vbox.setPadding(new Insets(10, 0, 0, 10));
         vbox.getChildren().addAll(table, hb);
-
-//        StackPane root = new StackPane();
-//        root.getChildren().add(table);
-
-        Scene scene = new Scene(vbox, 600, 600);
-        primaryStage.setTitle("Edit the database!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
